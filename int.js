d0df6fa441c08de415a266e942be4b72067d9b80
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-03 20:29:57 +0100

    add an api from wheretheiss website and edithed the css

--- int.js
+++ int.js
@@ -1,9 +1,15 @@
-// var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('issMap').setView([51.505, -0.09], 1);
 
 // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     maxZoom: 19,
 //     attribution: '© OpenStreetMap'
 // }).addTo(map);
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(map);
 
 const long = document.getElementById("long");
 const lat = document.getElementById("lat");
@@ -14,7 +20,7 @@ let getISS = async ()=>{
     let response = await fetch(url);
     let data = await response.json();
     const {latitude, longitude} = data;
-    console.log(latitude, longitude);
+    console.log(latitude, longitude); 
     // the code below shows the result(latitude, longitude) in the dom
     long.innerHTML = longitude + " long";
     lat.innerHTML = latitude +" lat";
