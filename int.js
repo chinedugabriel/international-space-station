d7dba9bdcab77c68f8152b782b15bebf98796f9b
chinedugabriel <chinedugabriel29@gmail.com>
2022-08-03 23:13:50 +0100

    added an icon and setintervale of 1 sec

--- int.js
+++ int.js
@@ -1,33 +1,56 @@
-var map = L.map('issMap').setView([51.505, -0.09], 1);
+// this Api was gotten from https://leafletjs.com/
+
+var map = L.map('issMap').setView([0, 0], 1);//this is a map object .setView([latitude, longitude], zoomLevel)
+
+var myIcon = L.icon({//making a maker with a custom icon
+    iconUrl: 'spaceX.png',
+    iconSize: [38, 75],
+    iconAnchor: [22, 94],
+
+});
+
+const marker = L.marker([0, 0], {icon: myIcon}).addTo(map);
 
 // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     maxZoom: 19,
 //     attribution: '© OpenStreetMap'
 // }).addTo(map);
 
+// this specifies the tiles which represnt the map images that's displayed
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 const tiles = L.tileLayer(tileUrl, {attribution});
 tiles.addTo(map);
 
+// got the divs that displays the longitude(long) and latitude(Lat)
 const long = document.getElementById("long");
 const lat = document.getElementById("lat");
 
+// API URL
 let url = "https://api.wheretheiss.at/v1/satellites/25544";
 
-let getISS = async ()=>{
+let firstTime = true;
+
+let getISS = async ()=>{// this function gets the data and converts it into a useable and readable data
     let response = await fetch(url);
     let data = await response.json();
-    const {latitude, longitude} = data;
-    console.log(latitude, longitude); 
+    const {latitude, longitude} = data; // deconstruction
+    // console.log(latitude, longitude); 
     // the code below shows the result(latitude, longitude) in the dom
-    long.innerHTML = longitude + " long";
-    lat.innerHTML = latitude +" lat";
+    long.innerHTML = longitude.toFixed(2) + " long";
+    lat.innerHTML = latitude.toFixed(2) +" lat";
+
+    // L.marker([50.5, 30.5]).addTo(map);
+    marker.setLatLng([latitude, longitude]);
+    if(firstTime){
+        map.setView([latitude, longitude], 1.5);
+        firstTime = false;
+    }
 
 };
 
-
-getISS();
-
+// calls the function
+// getISS();
+setInterval(getISS,1000)
 
